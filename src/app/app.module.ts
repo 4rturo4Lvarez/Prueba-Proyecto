f7e61174ccf280a1235eb250b70f65a54a8b17dd
4rturo4lvarez <t1023600321@unitru.edu.pe>
2022-12-12 20:20:45 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { GraficoBarrasComponent } from './componentes/grafico-barras/grafico-barras.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, GraficoBarrasComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
